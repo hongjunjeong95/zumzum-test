34a3053265034b560b8240be38fa88cc09d3ca3b
hongjunjeong <hongjunjeong95@gmail.com>
2023-12-27 17:24:45 +0900

    Add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 
@@ -14,7 +14,14 @@ async function bootstrap() {
   const options = new DocumentBuilder().setTitle('backend-test').build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
+
   app.use(cookieParser());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
 
   await app.listen(PORT || 3000, () => {
     if (process.send) {
